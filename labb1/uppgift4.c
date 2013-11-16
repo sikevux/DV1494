17fa70c3e175828ae4490e4d3a2f2b3435a641bc
Patrik Greco <sikevux@sikevux.se>
2013-11-16 13:33:19 +0100

    Added uppg4 labb1

---
+++ labb1/uppgift4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	int side1 = 10;
+	int side2 = 30;
+	int area = side1 * side2;
+	int circumference = (side1 * 2) + (side2 * 2);
+
+	printf("Rektanglen har sidorna %d och %d, vilket gör att arean blir %d och omkretsen %d\n", side1, side2, area, circumference);
+
+
+
+
+
+	return 0;
+}
